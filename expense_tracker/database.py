c5c65bf04bdb477d60cc10e380fc889715c6bc07
adarshvinod-devs <adarsh2047n@gmail.com>
2026-08-01 15:25:13 +0530

    Expense Tracker: completed 'database.py' module

--- expense_tracker/database.py
+++ expense_tracker/database.py
@@ -1,27 +1,42 @@
 import sqlite3
 
-connection = sqlite3.connect("expenses.db")
+def create_connection():
+    connection = sqlite3.connect("expenses.db")
+    cursor = connection.cursor()
+    return connection, cursor
 
-cursor = connection.cursor()
-
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS categories(
-        id INTEGER PRIMARY KEY,
-        category TEXT NOT NULL UNIQUE
-    )
-""")
-
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS expenses(
-        id INTEGER PRIMARY KEY,
-        amount REAL NOT NULL,
-        category_id INTEGER NOT NULL,
-        date TEXT NOT NULL,
-        description TEXT,
-        FOREIGN KEY (category_id) REFERENCES categories(id)
+def create_tables(cursor):
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS categories(
+            id INTEGER PRIMARY KEY,
+            category TEXT NOT NULL UNIQUE
         )
-""")
+    """)
 
-connection.commit()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS expenses(
+            id INTEGER PRIMARY KEY,
+            amount REAL NOT NULL,
+            category_id INTEGER NOT NULL,
+            date TEXT NOT NULL,
+            description TEXT,
+            FOREIGN KEY (category_id) REFERENCES categories(id)
+            )
+    """)
 
-connection.close()
+def insert_default_categories(cursor):
+    cursor.execute("SELECT COUNT(*) FROM categories")
+    result = cursor.fetchone()
+    if result[0]== 0:
+        categories_list = [ ("Food",),("Bills",),("Travel",),("Shopping",),("Health",),("Entertainment",),("Others",)]
+        cursor.executemany("INSERT INTO categories (category) VALUES (?)",categories_list)
+
+def setup_database():
+    connection , cursor = create_connection()
+    create_tables(cursor)
+    insert_default_categories(cursor)
+    connection.commit()
+    connection.close()
+
+if __name__ == "__main__":
+    setup_database()
